a8667bc53eb77afc8a85045cd8ce68730ee3bc3f
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-18 19:53:07 +0100

    created seeder with faker

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -16,14 +16,16 @@ class ProjectTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $new_project = new Project();
+        for ($i = 0; $i < 50; $i++) {
+            $new_project = new Project();
 
-        $new_project->name = $faker->words(2, true);
-        $new_project->slug = Project::generateSlug($new_project->name);
-        $new_project->client_name = $faker->words(2, true);
-        $new_project->summary = $faker->text();
-        $new_project->cover_image = 'https://www.innerintegratori.it/wp-content/uploads/2021/06/placeholder-image.png';
+            $new_project->name = $faker->words(2, true);
+            $new_project->slug = Project::generateSlug($new_project->name);
+            $new_project->client_name = $faker->words(2, true);
+            $new_project->summary = $faker->text();
+            $new_project->cover_image = 'https://www.innerintegratori.it/wp-content/uploads/2021/06/placeholder-image.png';
 
-        $new_project->save();
+            $new_project->save();
+        }
     }
 }
