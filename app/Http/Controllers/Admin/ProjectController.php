703efd6b0032fc075fcb7be1876eab6c81ebec2c
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-20 13:17:33 +0100

    [add] paginate method for $projects

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -15,7 +15,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(6);
         return view('admin.projects.index', compact('projects'));
     }
 
