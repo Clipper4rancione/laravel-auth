07fd1d068cd0bc94452b2153981cdcb1ad57e824
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-20 18:35:57 +0100

    cleared file from default old routes

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,5 @@ Route::middleware(['auth', 'verified'])
         Route::resource('projects', ProjectController::class);
     });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__ . '/auth.php';
