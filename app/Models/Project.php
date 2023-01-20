84cd9bbe4a4a4a9fd868c71cb601aedf57a4bd52
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-20 18:34:25 +0100

    [add] fillable camps

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,6 +10,8 @@ class Project extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'client_name', 'slug', 'cover_image', 'summary'];
+
     public static function generateSlug($string)
     {
         $slug = Str::slug($string, '-');
