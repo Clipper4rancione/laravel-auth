cadacd9ffbd34637ab9af773aa470eea0aa637e7
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-21 22:01:05 +0100

    deleted cover image col in table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="containerh-100">
-        <div class="table-container my-4 p-4">
+    <div class="container h-100">
+        <div class="table-container my-4 p-3">
             <table class="table table-striped">
                 <thead>
                     <tr>
@@ -10,7 +10,6 @@
                         <th scope="col">Name</th>
                         <th scope="col">Client name</th>
                         <th scope="col">Summary</th>
-                        <th scope="col">Cover Image</th>
                         <th scope="col">Actions</th>
                     </tr>
                 </thead>
@@ -21,11 +20,21 @@
                             <td>{{ $project->name }}</td>
                             <td>{{ $project->client_name }}</td>
                             <td>{{ $project->summary }}</td>
-                            <td>{{ $project->cover_image }}</td>
-                            <td>
+                            <td class="">
                                 <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-success"><i
                                         class="fa-solid fa-eye"></i></a>
                             </td>
+                            <td>
+                                <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-warning"><i
+                                        class="fa-solid fa-trash"></i></a>
+                            </td>
+                            <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <td>
+                                    <button class="btn btn-danger"><i class="fa-solid fa-trash" title="delete"></i></button>
+                                </td>
+                            </form>
                         </tr>
                     @endforeach
                 </tbody>
