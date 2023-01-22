41fc4066fff0d84fd2baf73f7090abad4a72827a
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-22 17:07:55 +0100

    [add] cover_image_original_name column

--- database/migrations/2023_01_18_181216_create_projects_table.php
+++ database/migrations/2023_01_18_181216_create_projects_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->string('slug', 130)->unique();
             $table->string('client_name', 90);
             $table->text('summary')->nullable();
-            $table->string('cover_image');
+            $table->string('cover_image')->nullable();
+            $table->string('cover_image_original_name')->nullable();
             $table->timestamps();
         });
     }
