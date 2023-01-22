db86cc344ec1926c8bcec711e1ef8956ec4f658d
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-22 17:07:25 +0100

    [add] original name of img in fillable array

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,7 +10,7 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'client_name', 'slug', 'cover_image', 'summary'];
+    protected $fillable = ['name', 'client_name', 'slug', 'cover_image', 'cover_image_original_name', 'summary'];
 
     public static function generateSlug($string)
     {
